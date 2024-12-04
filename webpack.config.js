0c448a5ce8935054fda7feeba2968b3b74039097
Mukhammadamin <mukhammadamin.sultonov@litmos.com>
2024-12-04 14:57:11 +0500

    add bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   entry: path.join(__dirname, "", "index.js"),
@@ -10,6 +12,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "public", "index.html"),
     }),
+    new BundleAnalyzerPlugin(),
   ],
   module: {
     rules: [
