3049b764d49e9626297f70c66f5f661f98443b1e
Mukhammadamin <mukhammadamin.sultonov@litmos.com>
2024-12-04 16:07:49 +0500

    test husky

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
+            <div>
+              <h1>Hello World!</h1>
+            </div>
+          );
 };
 
 export default App;
